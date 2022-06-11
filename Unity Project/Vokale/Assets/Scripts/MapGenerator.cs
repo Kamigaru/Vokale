07cd7d5ba132aedc1ac442ab79641d49295ab8c6
Kamigaru <bradsadams13@gmail.com>
2022-06-11 12:35:08 +0100

    Work so far
    lol

--- Unity Project/Vokale/Assets/Scripts/MapGenerator.cs
+++ Unity Project/Vokale/Assets/Scripts/MapGenerator.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public enum DrawMode { NoiseMap, ColourMap }
+    public enum DrawMode { NoiseMap, ColourMap, Mesh }
     public DrawMode drawmode;
 
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
+
+    public float treeNoiseScale;
+    public float treeDensity;
+
     public bool autoUpdate;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
 
     public int octaves;
     [Range(0,1)]
@@ -22,17 +29,51 @@ public class MapGenerator : MonoBehaviour
 
     public TerrainType[] regions;
 
+    bool spawnTree;
+    public GameObject TreePrefab;
+    public int treeSpacing;
+    public int currentTreeSpacing;
+
+    private void Start()
+    {
+        currentTreeSpacing = treeSpacing;
+    }
+
+    public void GenerateTree()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+        int treeIndex = 0;
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                if(noiseMap[x, y] > 0.4f && noiseMap[x,y] < 0.6f)
+                {
+                    float d = Random.Range(0f, treeDensity);
+                    if (noiseMap[x, y] < d)
+                    {
+                        treeIndex++;
+                        Instantiate(TreePrefab, new Vector2(x, y), Quaternion.identity);
+                    }
+                }
+            }
+        }
+
+        Debug.Log($"There are {treeIndex} trees on the map seed {seed}");
+    }
+
     public void generateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
-
         Color[] colourMap = new Color[mapWidth * mapHeight];
+        int treeIndex = 0;
         for(int y = 0; y < mapHeight; y++)
         {
             for(int x = 0; x < mapWidth; x++)
             {
                 float currentHeight = noiseMap[x, y];
+                //float TreeMap = TreenoiseMap[x, y];
                 for(int i = 0; i < regions.Length; i++)
                 {
                     if(currentHeight <= regions[i].height)
@@ -40,9 +81,21 @@ public class MapGenerator : MonoBehaviour
                         colourMap[y * mapWidth + x] = regions[i].colour;
                         break;
                     }
+
+                    /*if(TreeMap >= 0.6f && currentTreeSpacing <= 0)
+                    {
+                        treeIndex++;
+                        currentTreeSpacing = treeSpacing;
+                    }
+                    else
+                    {
+                        currentTreeSpacing--;
+                    }*/
                 }
             }
         }
+
+        Debug.Log($"There are {treeIndex} spawns for this seed: {seed}");
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         if(drawmode == DrawMode.NoiseMap)
@@ -52,6 +105,12 @@ public class MapGenerator : MonoBehaviour
         {
             display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
         }
+        else if(drawmode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrtainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+
+        GenerateTree();
     }
 
     void OnValidate()
